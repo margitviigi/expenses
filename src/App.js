c614d508b10f03ceff53576ebb6b41822df1d90e
Margit Viigi <margit.viigi@voco.ee>
2024-11-22 11:13:35 +0200

    Get data via props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
-function App() {
+const App = () =>{
+  const expenses = [
+    {
+    date: new Date(2024, 10, 22),
+    title:'New book',
+    price: 39.99
+    },
+    {
+    date: new Date(2024, 10, 22),
+    title:'New bag',
+    price: 99.99
+    },  
+] 
   return (
     <div className="App">
-      <ExpenseItem />
-      <ExpenseItem />
+      <ExpenseItem data={expenses[0]} />
+      <ExpenseItem data={expenses[1]} />
     </div>
   );
 }
